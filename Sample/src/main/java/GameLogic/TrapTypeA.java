ae9fda5478d8dc00d350199a66fbd55da3b68e99
Jason Tan <jasontan@Jasons-MacBook-Pro-2.local>
2020-03-16 13:43:00 -0700

    Removed Redundant Function and Code Cleanup in Trap

--- Sample/src/main/java/GameLogic/TrapTypeA.java
+++ Sample/src/main/java/GameLogic/TrapTypeA.java
@@ -5,7 +5,6 @@ public class TrapTypeA extends Trap {
     private  char type = 'A';
 
     public TrapTypeA() {super(new int[] {0,0}); }
-    public TrapTypeA(int[] position) {super(position); }
 
     public int getDamage() { return damage; }
     public char getType() { return type; }
--- Sample/src/main/java/GameLogic/TrapTypeB.java
+++ Sample/src/main/java/GameLogic/TrapTypeB.java
@@ -5,7 +5,6 @@ public class TrapTypeB extends Trap {
     private  char type = 'B';
 
     public TrapTypeB() {super(new int[] {0,0}); }
-    public TrapTypeB(int[] position) {super(position); }
     public int getDamage() { return damage; }
     public char getType() { return type; }
 
