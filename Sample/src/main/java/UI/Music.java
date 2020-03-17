136b3c2174fc7e95878bd31ec262dd02b520b885
Jason Tan <jasontan@Jasons-MacBook-Pro-2.local>
2020-03-17 01:48:42 -0700

    Code cleanup for Music

--- Sample/src/main/java/UI/Music.java
+++ Sample/src/main/java/UI/Music.java
@@ -3,7 +3,6 @@ package UI;
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
-import javax.swing.*;
 import java.io.File;
 
 public class Music {
