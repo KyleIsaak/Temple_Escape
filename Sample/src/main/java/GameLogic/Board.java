edfa74188760c5ddcd1e4d1af6935f6aa4a329b7
Jason Tan <jasontan@Jasons-MacBook-Pro-2.local>
2020-03-18 16:31:30 -0700

    Added JAVAdocs for Board class and its method

--- Sample/src/main/java/GameLogic/Board.java
+++ Sample/src/main/java/GameLogic/Board.java
@@ -4,23 +4,28 @@ package GameLogic;
 import java.util.ArrayList;
 import java.util.Random;
 
+/**
+ * Store the dimension of the Game Play Area.
+ * Manages all movable and non-movable objects in the Game Play Area.
+ */
 public class Board {
 
     private int sizeX = 27;
     private int sizeY = 27;
     private Player player;
     private int[] playerInit;
-
     private Score score;
-
     private ArrayList<Trap> trapArrayManager;
     private ArrayList<Reward> rewardArrayManager;
     private ArrayList<Enemy> EnemyArrayManager;
-
     private Exit exit;
     private LevelGenerator generator;
     //private static Timer timer;
 
+    /**
+     * Non-Default Constructor (Board Class)
+     * @param level An integer defining the difficulty selected.
+     */
     public Board(int level){
         generator = new LevelGenerator(sizeX, sizeY);
 
@@ -29,7 +34,7 @@ public class Board {
         rewardArrayManager = new ArrayList<Reward>();
         rewardGenerator(level);
         EnemyArrayManager = new ArrayList<Enemy>();
-        EnemyGenerator(level);
+        enemyGenerator(level);
 
         exit = new Exit();
         randomizeExitPosition();
@@ -41,7 +46,12 @@ public class Board {
         //timer = new Timer();
     }
 
-    //Non-Default Constructor
+    /**
+     * Non Default Constructor (Board Class)
+     * To be used for recreating board when transiting to next level.
+     * @param level An integer defining the difficulty selected.
+     * @param scoreAmount An integer holding the current score from the current level
+     */
     public Board(int level, int scoreAmount){
         generator = new LevelGenerator(sizeX, sizeY);
 
@@ -50,74 +60,37 @@ public class Board {
         rewardArrayManager = new ArrayList<Reward>();
         rewardGenerator(level);
         EnemyArrayManager = new ArrayList<Enemy>();
-        EnemyGenerator(level);
-
+        enemyGenerator(level);
         exit = new Exit();
         randomizeExitPosition();
-
         playerInit = new int[]{1, 1};
         player = new Player(playerInit);
-
         score = new Score(scoreAmount);
         //timer = new Timer();
     }
 
-    public boolean isWall(int x, int y){ return generator.isWall(x, y);}
-    public boolean isInBounds(int x, int y) {return (x >= 0 && x < sizeX && y >= 0 && y < sizeY);}
+    /**
+     * Check whether that location on the map contains a wall.
+     * @param x An integer for the x position.
+     * @param y An integer for the y position
+     * @return true when it contains a wall and vise versa.
+     */
+    public boolean isWall(int x, int y){ return generator.isWall(x, y); }
 
-    public int[][] getBoard() { return generator.getBoard();}
-    public Exit getExit() { return exit; }
-    public void setDifficulty(LevelGenerator.Difficulty choice) { generator.setLevel(choice);}
+    /**
+     * Check whether that location on the map is inside the boundary of the game play area.
+     * @param x An integer for the x position
+     * @param y An integer for the y position
+     * @return true when the location is inside the boundary and vise versa.
+     */
+    public boolean isInBounds(int x, int y) { return (x >= 0 && x < sizeX && y >= 0 && y < sizeY); }
 
-    public Player getPlayer(){return this.player;}
-    public int[] getPlayerPos(){ return player.getPosition(); }
-    public Enemy getEnemy(int i){return EnemyArrayManager.get(i);}
-    public int[] getEnemyPos(int i){return EnemyArrayManager.get(i).getPosition();}
-    public Score getScore() { return score; }
-
-    public int integerRandomizer(){
-        Random random = new Random();
-        return (random.nextInt(sizeX));
-    }
-
-    ///////////////// Trap Functionality ///////////////
-    public ArrayList<Trap> getTrapArrayManager(){ return trapArrayManager; }
-    public void trapGenerator (int difficultyLevel){
-        for (int i = 0; i < difficultyLevel*2; i++) {   //Spikes
-            trapTypeGenerator('A');
-        }
-        for (int i = 0; i < difficultyLevel; i++){      //Lava
-            trapTypeGenerator('B');
-        }
-
-    }
-
-    private void trapTypeGenerator (char type){
-        switch (type){
-            case 'A' :
-                trapLocationRandomizer(new TrapTypeA());
-                break;
-
-            case 'B' :
-                trapLocationRandomizer(new TrapTypeB());
-                break;
-        }
-    }
-
-    public void trapLocationRandomizer (Trap trapObject){
-        int x = integerRandomizer();
-        int y = integerRandomizer();
-
-        while ((isWall(x,y)) || (isTrap(x,y))){
-            x = integerRandomizer();
-            y = integerRandomizer();
-        }
-
-        trapObject.setPosition(new int [] {x, y});
-        //cellStatusManager.add(new int [] {x, y});
-        trapArrayManager.add(trapObject);
-    }
-
+    /**
+     * Check whether that location on the map contains a trap.
+     * @param x An integer for the x position.
+     * @param y An integer for the y position
+     * @return true when it contains a trap and vise versa.
+     */
     public boolean isTrap(int x, int y){
         for (int i = 0; i < trapArrayManager.size(); i++) {
             int[] current = trapArrayManager.get(i).getPosition();
@@ -128,6 +101,168 @@ public class Board {
         return false;
     }
 
+    /**
+     * Check whether that location on the map contains an enemy.
+     * @param x An integer for the x position.
+     * @param y An integer for the y position
+     * @return true when it contains an enemy and vise versa.
+     */
+    public boolean isEnemy(int x, int y) {
+        for (int i = 0; i < EnemyArrayManager.size(); i++) {
+            int[] current = EnemyArrayManager.get(i).getPosition();
+            if ((current[0] == x) && (current[1] == y)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * Check whether that location on the map contains a reward.
+     * @param x An integer for the x position.
+     * @param y An integer for the y position
+     * @return true when it contains a reward and vise versa.
+     */
+    public boolean isReward(int x, int y){
+        for (int i = 0; i < rewardArrayManager.size(); i++) {
+            int[] current = rewardArrayManager.get(i).getPosition();
+            if ((current[0] == x) && (current[1] == y)) {
+                return true;
+            }
+        }
+        return false;
+    }
+    /**
+     * Get this Board class's Reward Array Manager.
+     * @return this Board class's Reward Array Manager.
+     */
+    public ArrayList<Reward> getRewardArrayManager(){ return rewardArrayManager; }
+
+    /**
+     * Gets the board of the Generator class
+     * @return Generator's Board
+     */
+    public int[][] getBoard() { return generator.getBoard(); }
+
+    /**
+     * Gets the Exit of this Board
+     * @return this Board's Exit
+     */
+    public Exit getExit() { return exit; }
+
+    /**
+     * Set the difficulty of the game.
+     * @param choice Difficulty of the Game.
+     */
+    public void setDifficulty(LevelGenerator.Difficulty choice) { generator.setLevel(choice); }
+
+    /**
+     * Get the Player of this board.
+     * @return this Board's Player.
+     */
+    public Player getPlayer(){ return this.player; }
+
+    /**
+     * Get the Position of this Board's Player
+     * @return the position of this Board's Player.
+      */
+    public int[] getPlayerPos(){ return player.getPosition(); }
+
+    /**
+     * Get this Board class's Enemy Array Manager.
+     * @return this Board class's Enemy Array Manager.
+     */
+    public ArrayList<Enemy> getEnemyArrayManager() { return EnemyArrayManager; }
+
+    /**
+     * Get the ith index's Enemy from the Enemy Array
+     * @param i index of the Enemy located in the array
+     * @return the ith index's Enemy from the Enemy Array
+     */
+    public Enemy getEnemy(int i){ return EnemyArrayManager.get(i); }
+
+    /**
+     * Get the ith index's Enemy's position from the Enemy Array
+     * @param i index of the Enemy located in the array
+     * @return the ith index's Enemy's position from the Enemy Array
+     */
+    public int[] getEnemyPos(int i){ return EnemyArrayManager.get(i).getPosition(); }
+
+    /**
+     * Get the score of this Board class
+     * @return this Board class's Score
+     */
+    public Score getScore() { return score; }
+
+    /**
+     * Integer Randomizer with the bound of sizeX
+     * @return an randomized integer with the bound of sizeX
+     */
+    public int integerRandomizer(){
+        Random random = new Random();
+        return (random.nextInt(sizeX));
+    }
+
+    /**
+     * Get the Trap Array Manager of this Board Class
+     * @return this Board class's Trap Array Manager
+     */
+    public ArrayList<Trap> getTrapArrayManager(){ return trapArrayManager; }
+
+    /**
+     * Create Traps (Lava Pit and Spikes) in the gameplay area depending on the difficulty selected.
+     * @param difficultyLevel an integer that specify the difficulty selected.
+     */
+    public void trapGenerator (int difficultyLevel){
+        for (int i = 0; i < difficultyLevel*2; i++) {   //Spikes
+            trapTypeGenerator('A');
+        }
+        for (int i = 0; i < difficultyLevel; i++){      //Lava Pits
+            trapTypeGenerator('B');
+        }
+
+    }
+
+    /**
+     * trapGenerator's Helper function: Facilitates in creating the specific trap (Spikes and Lava Pits)
+     * Implements the factory method design pattern.
+     * @param type A character that specify the type of specific trap selected.
+     */
+    private void trapTypeGenerator (char type){
+        switch (type){
+            case 'A' :  //Spikes
+                trapLocationRandomizer(new TrapTypeA());
+                break;
+
+            case 'B' :  //Lava Pits
+                trapLocationRandomizer(new TrapTypeB());
+                break;
+        }
+    }
+
+    /**
+     * trapGenerator's Helper Function: Facilitates in the randomizing the location of the traps
+     * @param trapObject Specific Trap Object
+     */
+    public void trapLocationRandomizer (Trap trapObject){
+        int x = integerRandomizer();
+        int y = integerRandomizer();
+
+        while ((isWall(x,y)) || (isTrap(x,y))){
+            x = integerRandomizer();
+            y = integerRandomizer();
+        }
+
+        trapObject.setPosition(new int[]{x, y});
+        trapArrayManager.add(trapObject);
+    }
+
+    /**
+     * Returns the index of the Trap in the Trap Array Manager which is located in that position
+     * @param x An integer for the x position.
+     * @param y An integer for the y position.
+     * @return the index of the Trap of that position in the Trap Array Manager
+     */
     public int trapFinder (int x, int y){
         for (int i = 0; i < trapArrayManager.size(); i++){
             Trap current = trapArrayManager.get(i);
@@ -138,10 +273,9 @@ public class Board {
         return -1;
     }
 
-
-
-
-    ////////////// Exit /////////////
+    /**
+     * Randomize the position of this Board class's exit.
+     */
     public void randomizeExitPosition(){
         int x = integerRandomizer();
         int y = integerRandomizer();
@@ -154,6 +288,10 @@ public class Board {
 
     }
 
+    /**
+     * Check whether this Board class's exit is unlock
+     * @return true if this Board class's exit is unlock or vise versa
+     */
     public boolean isExitUnlocked(){
         if (exit.getIsUnlocked()){
             return true;
@@ -161,12 +299,19 @@ public class Board {
         return false;
     }
 
+    /**
+     * Unlock this Board class's exit.
+     */
     public void unlockExit(){
         exit.setUnlocked(true);
         System.out.println("Exit unlocked");
     }
 
-
+    /**
+     * Check whether the position is at a corner of a wall in the map.
+     * @param position Position in the Map
+     * @return true if the position is at a corner of a wall in the map and vise versa.
+     */
     public boolean isCorner(int[] position)
     {
         int X=position[0];
@@ -182,6 +327,11 @@ public class Board {
         return false;
     }
 
+    /**
+     * Check whether the position is at a corner of a wall in the map.
+     * @param position Position in the Map
+     * @return true if the position is at a corner of a wall in the map and vise versa.
+     */
     public boolean isThreeWall(int[]position)
     {
         int X=position[0];
@@ -197,10 +347,11 @@ public class Board {
         return false;
     }
 
-
-
-
-    //Enemy functionality
+    /**
+     * Provide enemy functionality of approaching the player using the nearest path
+     * @param enemy The enemy that is chasing the player.
+     * @param i The index of that enemy in the Enemy Array Manager.
+     */
     public void chaseThePlayer(Enemy enemy, int i){
         boolean testValidMove = true;
         int[] planMove = {0,0};
@@ -310,27 +461,29 @@ public class Board {
         enemy.move(planMove);
     }
 
-
-    public ArrayList<Enemy> getEnemyArrayManager() {
-        return EnemyArrayManager;
-    }
-
-
-    public void EnemyGenerator(int difficultyLevel)
+    /**
+     * Create Enemy in the gameplay area depending on the difficulty selected.
+     * @param difficultyLevel An integer of the difficulty selected.
+     */
+    public void enemyGenerator(int difficultyLevel)
     {
         if(difficultyLevel < 3) {
             for (int i = 0; i < difficultyLevel; i++) {
-                EnemyLocationRandomizer(new Enemy(playerInit));
+                enemyLocationRandomizer(new Enemy(playerInit));
             }
         }
         else{
             for (int i = 0; i < 3; i++) {
-                EnemyLocationRandomizer(new Enemy(playerInit));
+                enemyLocationRandomizer(new Enemy(playerInit));
             }
         }
     }
 
-    public void EnemyLocationRandomizer(Enemy enemy) {
+    /**
+     *  EnemyGenerator's Helper Function: Facilitates in the randomizing the location of the enemy
+     * @param enemy A specific enemy object
+     */
+    public void enemyLocationRandomizer(Enemy enemy) {
         int x = integerRandomizer();
         int y = integerRandomizer();
 
@@ -344,16 +497,12 @@ public class Board {
         EnemyArrayManager.add(enemy);
     }
 
-    public boolean isEnemy(int x, int y) {
-        for (int i = 0; i < EnemyArrayManager.size(); i++) {
-            int[] current = EnemyArrayManager.get(i).getPosition();
-            if ((current[0] == x) && (current[1] == y)) {
-                return true;
-            }
-        }
-        return false;
-    }
-
+    /**
+     * Returns the index of the Enemy in the Enemy Array Manager which is located in that position
+     *  @param x An integer for the x position.
+     *  @param y An integer for the y position.
+     *  @return the index of the Enemy of that position in the Enemy Array Manager
+     */
     public int EnemyFinder(int x, int y) {
         for (int i = 0; i < EnemyArrayManager.size(); i++) {
             Enemy current = EnemyArrayManager.get(i);
@@ -364,6 +513,10 @@ public class Board {
         return -1;
     }
 
+    /**
+     * Move the enemy by one step in either direction.
+     * @param currentlevel An integer of the current difficulty selected.
+     */
     public void MoveEnemy(int currentlevel) {
         for (int i = 0; i < currentlevel; i++) {
             int[] position = EnemyArrayManager.get(i).chaseThePlayer(getPlayerPos());
@@ -381,23 +534,24 @@ public class Board {
         }
     }
 
-
-
-
-    ///////////////// Reward Functionality ///////////////
-    public ArrayList<Reward> getRewardArrayManager(){ return rewardArrayManager; }
+    /**
+     * Create Specific Rewards in the gameplay area depending on the difficulty selected.
+     * @param difficultyLevel An integer of the difficulty selected.
+     */
     public void rewardGenerator (int difficultyLevel){
-
         for (int i = 0; i < difficultyLevel*2; i++) {   //Coins
             rewardTypeGenerator('A');
         }
         for (int i = 0; i < difficultyLevel; i++){      //Keys
             rewardTypeGenerator('B');
         }
-        //regularRewardCounter = 4;
-
     }
 
+    /**
+     * rewardGenerator's Helper function: Facilitates in creating the specific rewards (Coins and Keys)
+     * Implements the factory method design pattern.
+     * @param type A character that specify the type of specific reward selected.
+     */
     private void rewardTypeGenerator (char type){
         switch (type){
             case 'A' :
@@ -410,6 +564,10 @@ public class Board {
         }
     }
 
+    /**
+     * rewardGenerator's Helper Function: Facilitates in the randomizing the location of the rewards
+     * @param rewardObject Specific Reward Object
+     */
     public void rewardLocationRandomizer (Reward rewardObject){
         int x = integerRandomizer();
         int y = integerRandomizer();
@@ -424,16 +582,12 @@ public class Board {
         rewardArrayManager.add(rewardObject);
     }
 
-    public boolean isReward(int x, int y){
-        for (int i = 0; i < rewardArrayManager.size(); i++) {
-            int[] current = rewardArrayManager.get(i).getPosition();
-            if ((current[0] == x) && (current[1] == y)) {
-                return true;
-            }
-        }
-        return false;
-    }
-
+    /**
+     * Returns the index of the Reward in the Reward Array Manager which is located in that position
+     *  @param x An integer for the x position.
+     *  @param y An integer for the y position.
+     *  @return the index of the Reward of that position in the Reward Array Manager
+     */
     public int rewardFinder (int x, int y){
         for (int i = 0; i < rewardArrayManager.size(); i++){
             Reward current = rewardArrayManager.get(i);
