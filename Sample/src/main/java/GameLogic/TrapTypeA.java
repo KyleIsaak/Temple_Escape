30b069c1fc09063a9f30aba11de2da427debf942
Jason Tan <jasontan@Jasons-MacBook-Pro-2.local>
2020-03-18 19:51:33 -0700

    Code Clean Up and Added JAVADocs for Trap Type A

--- Sample/src/main/java/GameLogic/TrapTypeA.java
+++ Sample/src/main/java/GameLogic/TrapTypeA.java
@@ -1,12 +1,28 @@
 package GameLogic;
 
+/**
+ * Represents a specific kind of Traps (Spikes) in the game.
+ * Player lose points when stepped on traps.
+ */
 public class TrapTypeA extends Trap {
     private int damage = 10;
     private  char type = 'A';
 
+    /**
+     * Default Constructor
+     */
     public TrapTypeA() {super(new int[] {0,0}); }
 
+    /**
+     * Get the damage of this trap.
+     * @return the damage of this trap.
+     */
     public int getDamage() { return damage; }
+
+    /**
+     * Get the type of this Trap which is A (Spikes)
+     * @return the type of this Trap which is A (Spikes)
+     */
     public char getType() { return type; }
 
 }
