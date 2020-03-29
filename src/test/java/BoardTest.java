035e5ec6a71ef2b7d2e13051245a0f729f25821c
Jason Tan <jasontan@Jasons-MacBook-Pro-2.local>
2020-03-29 16:15:32 -0700

    Added TestCase for the number of traps Created in the Board depending on level selected.

---
+++ src/test/java/BoardTest.java
@@ -0,0 +1,41 @@
+import GameLogic.*;
+import UI.*;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EnumSource;
+import org.junit.jupiter.params.provider.ValueSource;
+import static org.junit.jupiter.api.Assertions.*;
+
+
+public class BoardTest{
+
+    @ParameterizedTest
+    @ValueSource (ints = {1, 2 ,3})
+    void numberOfTrapACreatedBasedOnLevel (int level){
+        Board board = new Board(level);
+        int numberofTrapACreated = 0;
+        for (int i = 0; i < board.getTrapArrayManager().size(); i++){
+            if (board.getTrapArrayManager().get(i).getType() == 'A'){
+                numberofTrapACreated++;
+            }
+        }
+        assertEquals(level * 2, numberofTrapACreated);
+    }
+
+    @ParameterizedTest
+    @ValueSource (ints = {1, 2 ,3})
+    void numberOfTrapBCreatedBasedOnLevel (int level){
+        Board board = new Board(level);
+        int numberofTrapBCreated = 0;
+        for (int i = 0; i < board.getTrapArrayManager().size(); i++){
+            if (board.getTrapArrayManager().get(i).getType() == 'B'){
+                numberofTrapBCreated++;
+            }
+        }
+        assertEquals(level, numberofTrapBCreated);
+    }
+
+
+}
+
