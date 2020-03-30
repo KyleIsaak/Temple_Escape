c8fe6cbbe89584df41e556fe5d51494c65d7cab2
Jason Tan <jasontan@Jasons-MacBook-Pro-2.local>
2020-03-29 19:17:22 -0700

    Added Unit Test for Trap Class

--- src/test/java/GameLogic/BoardTest.java
+++ src/test/java/GameLogic/BoardTest.java
@@ -36,5 +36,33 @@ public class BoardTest{
         }
         assertEquals(level, numberofTrapBCreated);
     }
+
+    @Test
+    void trapLocationBounds (){
+        Board board = new Board(3);
+        boolean InBounds = true;
+        for (int i = 0; i < board.getTrapArrayManager().size(); i++){
+            int[] position = board.getTrapArrayManager().get(i).getPosition();
+            if ((position[0] < 0 || position[0] >= 27) || (position[1] < 0 || position[1] >= 27)){
+                InBounds = false;
+            }
+        }
+        assertEquals(true, InBounds);
+    }
+
+     @Test
+    void totalDamageDeductionCheck (){
+        Board board = new Board(1);
+        int intialScore = 500;
+        for (int i = 0; i < 27; i++){
+            for (int j = 0; j < 27; j++){
+                if (board.isTrap(i,j)){
+                    int trapIndex = board.trapFinder(i, j);
+                    intialScore = intialScore - board.getTrapArrayManager().get(trapIndex).getDamage();
+                }
+            }
+        }
+        assertEquals(0,intialScore);
+    }
 }
 
