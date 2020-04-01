9c70ce028b4c73f65ac7b0f49426e7d2119a1b99
KyleIsaak <55521911+KyleIsaak@users.noreply.github.com>
2020-04-01 16:41:21 -0700

    made timer tests

---
+++ src/test/java/GameLogic/TimerTest.java
@@ -0,0 +1,55 @@
+package GameLogic;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import java.util.concurrent.TimeUnit;
+
+class TimerTest {
+
+    @Test
+    public void getSeconds() {
+        long delay = 1;
+        Timer timer = new Timer();
+        long startTime = System.currentTimeMillis();
+
+        //TimeUnit.SECONDS.sleep(delay);
+        try {
+            TimeUnit.SECONDS.sleep(3);
+        } catch (InterruptedException ie)
+        {
+            Thread.currentThread().interrupt();
+        }
+        long testTime = timer.getSeconds();
+        long endTime = System.currentTimeMillis();
+        long totalTime = endTime - startTime;
+
+        assertEquals(totalTime / 1000, testTime);
+    }
+
+    @Test
+    public void pauseTimer() {
+        long delay = 1;
+        Timer timer = new Timer();
+
+        timer.pauseTimer();
+
+        try {
+            TimeUnit.SECONDS.sleep(3);
+        } catch (InterruptedException ie)
+        {
+            Thread.currentThread().interrupt();
+        }
+
+        timer.resumeTimer();
+
+        try {
+            TimeUnit.SECONDS.sleep(2);
+        } catch (InterruptedException ie)
+        {
+            Thread.currentThread().interrupt();
+        }
+
+        assertEquals(2, timer.getSeconds());
+    }
+
+}
