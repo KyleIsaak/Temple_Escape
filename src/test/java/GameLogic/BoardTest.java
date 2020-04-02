ad398f6021b474c0a76c47e48f295ef3f4f0ce62
Jason Tan <jasontan@Jasons-MacBook-Pro-2.local>
2020-04-02 14:23:32 -0700

    Update POM.xml to support exec:java

--- src/test/java/GameLogic/BoardTest.java
+++ src/test/java/GameLogic/BoardTest.java
@@ -6,6 +6,9 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.EnumSource;
 import org.junit.jupiter.params.provider.ValueSource;
+
+import javax.swing.*;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 
@@ -13,27 +16,19 @@ public class BoardTest{
 
     @ParameterizedTest
     @ValueSource (ints = {1, 2 ,3})
-    void numberOfTrapACreatedBasedOnLevel (int level) {
+    void numberOfTrapsCreatedBasedOnLevel (int level) {
         Board board = new Board(level);
         int numberofTrapACreated = 0;
+        int numberofTrapBCreated = 0;
         for (int i = 0; i < board.getTrapArrayManager().size(); i++) {
             if (board.getTrapArrayManager().get(i).getType() == 'A') {
                 numberofTrapACreated++;
             }
-        }
-        assertEquals(level * 2, numberofTrapACreated);
-    }
-
-    @ParameterizedTest
-    @ValueSource (ints = {1, 2 ,3})
-    void numberOfTrapBCreatedBasedOnLevel (int level){
-        Board board = new Board(level);
-        int numberofTrapBCreated = 0;
-        for (int i = 0; i < board.getTrapArrayManager().size(); i++){
             if (board.getTrapArrayManager().get(i).getType() == 'B'){
                 numberofTrapBCreated++;
             }
         }
+        assertEquals(level * 2, numberofTrapACreated);
         assertEquals(level, numberofTrapBCreated);
     }
 
@@ -47,7 +42,7 @@ public class BoardTest{
                 InBounds = false;
             }
         }
-        assertEquals(true, InBounds);
+        assertTrue(InBounds);
     }
 
      @Test
@@ -128,7 +123,5 @@ public class BoardTest{
         }
         assertTrue(InBounds);
     }
-
-
 }
 
