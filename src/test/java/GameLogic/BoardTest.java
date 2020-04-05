191e4cc8737c1919bfbdcf5d6c957d82c5bc1be4
Alex Chou <hontatc@sfu.ca>
2020-04-04 20:08:47 -0700

    add part of the test case of enemy of class board

--- src/test/java/GameLogic/BoardTest.java
+++ src/test/java/GameLogic/BoardTest.java
@@ -9,11 +9,21 @@ import org.junit.jupiter.params.provider.ValueSource;
 
 import javax.swing.*;
 
+import java.util.ArrayList;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 
-public class BoardTest{
+public class BoardTest {
 
+   @Test
+    void Board_Non_default_Constructor_test() {
+        Board test =new Board(1,1000);
+        int[] testPlayerLocation={1,1};
+        assertEquals(1,Misc.getCurrentLevel());
+        assertEquals(100,Misc.getScoreContainer());
+        assertArrayEquals(testPlayerLocation,test.getPlayerPos());
+    }
     @ParameterizedTest
     @ValueSource (ints = {1, 2 ,3})
     void numberOfTrapsCreatedBasedOnLevel (int level) {
@@ -123,5 +133,102 @@ public class BoardTest{
         }
         assertTrue(InBounds);
     }
+
+
+    ////////////////Enemy test case/////////////////////
+    @ParameterizedTest
+    @ValueSource (ints = {1, 2 ,3,4,5})
+    void isEnemy_Test(int level){
+       //test base on the level
+       Board test=new Board(level);
+
+       //max enemy=3;
+       if (level>=4)
+           level=3;
+       Enemy enemy=test.getEnemyArrayManager().get(level-1);
+       assertEquals(true,test.isEnemy(enemy.getPosition()[0],enemy.getPosition()[1]));
+       //enemy cannot stay the player position
+       assertEquals(false,test.isEnemy(1,1));
+
+    }
+
+    @ParameterizedTest
+    @ValueSource (ints = {1, 2 ,3,4,5})
+    void getEnemy_test(int level)
+    {
+
+        Board board=new Board(level);
+        //max enemy=3;
+        if (level>=4)
+            level=3;
+        ArrayList<Enemy> test=board.getEnemyArrayManager();
+        for(int i=0;i<level;i++)
+            assertEquals(test.get(i),board.getEnemyArrayManager().get(i));
+    }
+
+    @ParameterizedTest
+    @ValueSource (ints = {1, 2 ,3,4,5})
+    void getEnemyPos(int level) {
+        Board board = new Board(level);
+        //max enemy=3;
+        if (level >= 4)
+            level = 3;
+        ArrayList<Enemy> test = board.getEnemyArrayManager();
+        for (int i = 0; i < level; i++) {
+            boolean test1 = true;
+            test1 = board.isEnemy(test.get(i).getPosition()[0], test.get(i).getPosition()[1]);
+            assertEquals(true, test1);
+        }
+
+    }
+    @ParameterizedTest
+    @ValueSource (ints = {1, 2 ,3,4,5})
+    //test enemyLocationRandomizer Function
+    void testEnemyLocationRandomizerInTheMaze (int level){
+        Board board = new Board(level);
+        boolean InBound = true;
+        for (int i = 0; i < board.getEnemyArrayManager().size(); i++){
+            int[] position = board.getEnemyArrayManager().get(i).getPosition();
+            if ((position[0] < 0 || position[0] >= 27) || (position[1] < 0 || position[1] >= 27)){
+                InBound = false;
+            }
+        }
+        assertEquals(true,InBound);
+    }
+
+    @ParameterizedTest
+    @ValueSource (ints = {1, 2 ,3,4,5,6})
+    //test function EnemyGenerator function
+    void numberOfEnemyCreatedBasedOnLevel (int level) {
+        Board board = new Board(level);
+        int numberofEnemyCreated = 0;
+        for (int i = 0; i <27; i++)
+        {
+            for(int j=0;j<27;j++)
+            {
+                if(board.isEnemy(i,j)==true)
+                    numberofEnemyCreated ++;
+            }
+        }
+        if(level>=4)
+            level=3;
+        assertEquals(level, numberofEnemyCreated);
+    }
+
+    @ParameterizedTest
+    @ValueSource (ints = {1, 2 ,3,4,5,6})
+    void EnemyFinderTest(int level)
+    {
+       Board board=new Board(level);
+       ArrayList<Enemy> test = board.getEnemyArrayManager();
+       for(int i=0;i<board.getEnemyArrayManager().size();i++)
+       {
+           int x=test.get(i).getPosition()[0];
+           int y=test.get(i).getPosition()[1];
+           int result=board.EnemyFinder(x,y);
+           assertEquals(result,i);
+       }
+    }
+
 }
 
