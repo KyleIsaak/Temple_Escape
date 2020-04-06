45e7015b90500d52c3d278605eeebd3340728033
Jason Tan <jasontan@Jasons-MacBook-Pro-2.local>
2020-04-05 18:49:06 -0700

    Added Reward and Enemy Pathway Test using BFS Algorithm

--- src/test/java/GameLogic/GeneratorTest.java
+++ src/test/java/GameLogic/GeneratorTest.java
@@ -13,7 +13,6 @@ public class GeneratorTest {
         return ((x >= 1 && x < 26) && (y >= 1 && y < 26)) && board[x][y] == 1;
     }
     private boolean BFS (int[][] board, int[] position){
-        System.out.println(position[0] +", " + position[1]);
         boolean[][] visitedNode = new boolean[27][27];
         for (int i = 0 ; i < 27; i ++){
             for (int j = 0; j < 27; j++){
@@ -88,6 +87,52 @@ public class GeneratorTest {
         testLocationRandomizerGenerator.objectGenerator(level,"Trap");
         for (int i = 0; i < testLocationRandomizerGenerator.getTrapArrayManager().size(); i++){
             assertTrue(BFS(testGenerator.getBoard(), testLocationRandomizerGenerator.getTrapArrayManager().get(i).getPosition()));
+        }
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2 ,3})
+    public void RewardBoardPathwayTest(int level){
+        LevelGenerator testGenerator = new LevelGenerator(27, 27);
+        switch (level){
+            case 1 :
+                testGenerator.setLevel(LevelGenerator.Difficulty.EASY);
+                break;
+            case 2 :
+                testGenerator.setLevel(LevelGenerator.Difficulty.MEDIUM);
+                break;
+            case 3 :
+                testGenerator.setLevel(LevelGenerator.Difficulty.HARD);
+                break;
+        }
+        LocationRandomizerGenerator testLocationRandomizerGenerator = new LocationRandomizerGenerator(testGenerator.getBoard());
+        testLocationRandomizerGenerator.randomizeExitPosition();
+        testLocationRandomizerGenerator.objectGenerator(level,"Reward");
+        for (int i = 0; i < testLocationRandomizerGenerator.getRewardArrayManager().size(); i++){
+            assertTrue(BFS(testGenerator.getBoard(), testLocationRandomizerGenerator.getRewardArrayManager().get(i).getPosition()));
+        }
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2 ,3})
+    public void EnemyBoardPathwayTest(int level){
+        LevelGenerator testGenerator = new LevelGenerator(27, 27);
+        switch (level){
+            case 1 :
+                testGenerator.setLevel(LevelGenerator.Difficulty.EASY);
+                break;
+            case 2 :
+                testGenerator.setLevel(LevelGenerator.Difficulty.MEDIUM);
+                break;
+            case 3 :
+                testGenerator.setLevel(LevelGenerator.Difficulty.HARD);
+                break;
+        }
+        LocationRandomizerGenerator testLocationRandomizerGenerator = new LocationRandomizerGenerator(testGenerator.getBoard());
+        testLocationRandomizerGenerator.randomizeExitPosition();
+        testLocationRandomizerGenerator.objectGenerator(level,"Enemy");
+        for (int i = 0; i < testLocationRandomizerGenerator.getEnemyArrayManager().size(); i++){
+            assertTrue(BFS(testGenerator.getBoard(), testLocationRandomizerGenerator.getEnemyArrayManager().get(i).getPosition()));
         }
     }
 }
