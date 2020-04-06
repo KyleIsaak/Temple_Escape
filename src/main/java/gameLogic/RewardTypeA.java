33260d95b4a8aff0499a35aa5cb66aefc4cde399
KyleIsaak <55521911+KyleIsaak@users.noreply.github.com>
2020-04-06 15:39:29 -0700

    updated coding style for RewardTypeA

--- src/main/java/gameLogic/RewardTypeA.java
+++ src/main/java/gameLogic/RewardTypeA.java
@@ -1,23 +1,26 @@
 package gameLogic;
 
 /**
- * Represents a specific kind of rewards (Keys) in the game
- * Player is required to collect all this rewards before can proceed to
- * next level
+ * Represents a specific kind of reward (Keys) in the game
+ * Player is required to collect all of these rewards before
+ * they can proceed to the next level.
  */
 public class RewardTypeA extends Reward {
-    private int rewardAmount = 50;
+    private final int rewardAmount = 50;
     private  char type = 'A';
 
     /**
-     * Default Constructor
+     * Default Constructor.
      */
-    public RewardTypeA() { super(new int[] {0,0}); }
+    public RewardTypeA() {
+        super(new int[] {0, 0});
+    }
 
     /**
-     * Non Default Constructor
+     * Non Default Constructor.
+     * @param pos An x,y position in the map where the reward will be created.
      */
-    public RewardTypeA(int[] pos) {
+    public RewardTypeA(final int[] pos) {
         this.setPosition(pos);
     }
 
@@ -25,12 +28,16 @@ public class RewardTypeA extends Reward {
      * Get the value of this reward.
      * @return the value of this reward.
      */
-    public int getRewardAmount(){ return this.rewardAmount; }
+    public int getRewardAmount() {
+        return this.rewardAmount;
+    }
 
     /**
-     * Get the type of this Rewards
+     * Get the type of this Reward.
      * @return the type of this rewards which is A (Keys).
      */
-    public char getType() { return type; }
+    public char getType() {
+        return type;
+    }
 
 }
