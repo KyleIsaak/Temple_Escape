afa228b92afad274eae16e89945f9001291771e6
Alex Chou <hontatc@sfu.ca>
2020-04-06 23:34:27 -0700

    code style changed

--- src/main/java/gameLogic/TrapArray.java
+++ src/main/java/gameLogic/TrapArray.java
@@ -3,10 +3,16 @@ package gameLogic;
 import java.util.ArrayList;
 
 /**
- * Implements the interface of ArrayFinderManager (Traps Objects)
+ * Implements the interface of ArrayFinderManager (Traps Objects).
  */
 public class TrapArray extends ArrayFinderManager {
-
+    /**
+     * testing the the given position is the given Object.
+     * @param x An integer for the x position.
+     * @param y An integer for the y position
+     * @param objectArray The array containing the Objects.
+     * @return true if same .
+     */
     public boolean isObject (int x, int y, ArrayList<?> objectArray){
         for (Object object : objectArray) {
             Trap currentTrap = (Trap) object;
@@ -18,6 +24,13 @@ public class TrapArray extends ArrayFinderManager {
         return false;
     }
 
+    /**
+     * find the object
+     * @param x An integer for the x position.
+     * @param y An integer for the y position.
+     * @param objectArray The array containing the Objects.
+     * @return the number of the object, else return -1.
+     */
     public int objectFinder (int x, int y, ArrayList<?> objectArray){
         for (int i = 0; i < objectArray.size(); i++){
             Trap currentTrap = (Trap) objectArray.get(i);
