d0bce973b17e7432dbd290bd8c6c22016be40f52
Alex Chou <hontatc@sfu.ca>
2020-04-06 23:30:11 -0700

    code style changed

--- src/main/java/gameLogic/Score.java
+++ src/main/java/gameLogic/Score.java
@@ -12,35 +12,41 @@ public class Score {
     /**
      * Default Constructor.
      */
-    public Score () { this.currentScore = 100; }
+    public Score () {
+        this.currentScore = 100; }
 
     /**
      * Non-Default Constructor.
      * @param score An integer to set the current score as.
      */
-    public Score(int score) { this.currentScore = score; }
+    public Score(int score) {
+        this.currentScore = score; }
 
     /**
      * Get the current score.
      * @return the current score.
      */
-    public int getScore() { return this.currentScore; }
+    public int getScore() {
+        return this.currentScore; }
 
     /**
      * Add points to current score.
      * @param scoreToAdd An integer of the points to be added.
      */
-    public void addScore(int scoreToAdd) { this.currentScore += scoreToAdd; }
+    public void addScore(int scoreToAdd) {
+        this.currentScore += scoreToAdd; }
 
     /**
      * Subtract points from the current score.
      * @param scoreToSubtract An integer of the point to be subtracted.
      */
-    public void subtractScore(int scoreToSubtract) { this.currentScore -= scoreToSubtract; }
+    public void subtractScore(int scoreToSubtract) {
+        this.currentScore -= scoreToSubtract; }
 
     /**
      * Check whether the current score is negative.
      * @return true is the current score is negative and vise versa.
      */
-    public boolean isNegative () { return currentScore < 0; }
+    public boolean isNegative () {
+        return currentScore < 0; }
 }
