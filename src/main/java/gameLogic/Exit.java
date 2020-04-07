f2c793e556d89912c72aeba3215e6359b1966a53
Alex Chou <hontatc@sfu.ca>
2020-04-06 23:19:15 -0700

    fixed exit.java code style

--- src/main/java/gameLogic/Exit.java
+++ src/main/java/gameLogic/Exit.java
@@ -4,19 +4,25 @@ package gameLogic;
  * Represent the Exit Door in the game.
  */
 public class Exit {
+    /**
+     * Store exit position.
+     */
     private int[] position;
+    /**
+     * Store exit is lock or not info.
+     */
     private boolean isUnlocked;
 
     /**
-     * Default Constructor
-     * isUnlocked set to false
+     * Default Constructor.
+     * isUnlocked set to false.
      */
     public Exit(){
         isUnlocked = false;
     }
 
     /**
-     * Non-Default Constructor
+     * Non-Default Constructor.
      * @param position A x, y position that specify the location of exit.
      */
     public Exit(int[] position){
@@ -24,23 +30,23 @@ public class Exit {
     }
 
     /**
-     * Get the position of the exit in the map
-     * @return the position of the exit in the map
+     * Get the position of the exit in the map.
+     * @return the position of the exit in the map.
      */
     public int[] getPosition(){
         return position.clone();
     }
 
     /**
-     * Get whether this exit is unlock
-     * @return true if it is unlocked or vise versa
+     * Get whether this exit is unlock.
+     * @return true if it is unlocked or vise versa.
      */
     public boolean getIsUnlocked(){
         return isUnlocked;
     }
 
     /**
-     * Set the position of the exit of the game
+     * Set the position of the exit of the game.
      * @param position A x, y position that specify the new position for the exit.
      */
     public void setPosition(int[] position){
@@ -48,8 +54,8 @@ public class Exit {
     }
 
     /**
-     * Set whether this exit is unlocked or locked
-     * @param isUnlocked A boolean that specify whether the door is locked or unlocked
+     * Set whether this exit is unlocked or locked.
+     * @param isUnlocked A boolean that specify whether the door is locked or unlocked.
      */
     public void setUnlocked(boolean isUnlocked) { this.isUnlocked = isUnlocked; }
 
